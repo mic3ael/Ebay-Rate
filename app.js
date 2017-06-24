15a0e46d51846593840f29f992b99268bdaac16f
Michael Horojanski <mic3ael@gmail.com>
2017-06-24 08:50:42 +0300

    added

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
 
 /*
