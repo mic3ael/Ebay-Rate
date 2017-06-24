be8926cc6b8154c25ecac1036506dd510a38b741
Michael Horojanski <mic3ael@gmail.com>
2017-06-24 08:49:14 +0300

    simple

--- app.js
+++ app.js
@@ -22,9 +22,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+/*
 app.use('/', index);
 app.use('/users', users);
-
+*/
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
