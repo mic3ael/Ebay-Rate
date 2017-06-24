b04638a3ed9e2b346936349861f71b877696b63f
Michael Horojanski <mic3ael@gmail.com>
2017-06-24 10:43:37 +0300

    asda

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.scss';
-//import axios from 'axios';
-
-/*axios.get('/users').then(function(response) {
-  console.log(response);
-}).catch(function(error) {
-  console.log(error);
-});*/
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
