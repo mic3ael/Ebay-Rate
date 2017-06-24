1573c726656d70397320c8280b2d596185867ce5
Michael Horojanski <mic3ael@gmail.com>
2017-06-24 09:13:07 +0300

    heroku

--- app.js
+++ app.js
@@ -21,10 +21,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+/*
 app.use('/', index);
 app.use('/users', users);
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
